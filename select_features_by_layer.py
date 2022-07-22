b2fdd4760ee63ad209cf1d421547e43749b54e84
Anthony Twesigye <twesigye10@gmail.com>
2022-07-22 09:57:24 +0300

    select by another layer

--- select_features_by_layer.py
+++ select_features_by_layer.py
@@ -3,9 +3,16 @@ import geopandas as gdp
 # buildings datasets
 df_data_19d = gdp.read_file(filename="outputs/19d_buildings.gpkg")
 
+print(len(df_data_19d))
+
 df_data_177 = gdp.read_file(filename="outputs/177_buildings.gpkg")
 
 # host community dataset
 
 df_data_host_community = gdp.read_file(filename="inputs/UGA_Host_subcounty.gpkg")
+
+# select by another layer
+df_selection_19d = df_data_19d.overlay(right=df_data_host_community, how="identity")
+df_selection_177 = df_data_177.overlay(right=df_data_host_community, how="identity")
+
 
