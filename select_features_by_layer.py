3374571d2f8af7679777d5ae3216289aaa5a2c8a
Anthony Twesigye <twesigye10@gmail.com>
2022-07-22 10:51:36 +0300

    reproject host dataset to the crs of buildings dataset

--- select_features_by_layer.py
+++ select_features_by_layer.py
@@ -3,17 +3,16 @@ import geopandas as gdp
 # buildings datasets
 df_data_19d = gdp.read_file(filename="outputs/19d_buildings.gpkg")
 
-print(len(df_data_19d))
-
 df_data_177 = gdp.read_file(filename="outputs/177_buildings.gpkg")
 
 # host community dataset
 
 df_data_host_community = gdp.read_file(filename="inputs/UGA_Host_subcounty.gpkg")
-
+df_data_host_community_reproject = df_data_host_community.to_crs(df_data_19d.crs)
 # select by another layer
-df_selection_19d = df_data_19d.overlay(right=df_data_host_community, how="identity")
-df_selection_177 = df_data_177.overlay(right=df_data_host_community, how="identity")
+df_selection_19d = df_data_19d.overlay(right=df_data_host_community_reproject, how="identity")
+
+df_selection_177 = df_data_177.overlay(right=df_data_host_community_reproject, how="identity")
 
 # write output
 df_selection_19d.to_file("outputs/19d_buildings_ref_host.gpkg", layer='19d_buildings_ref_host', driver="GPKG")
