cc749dc2552559c4418d781887ea54af18561093
Anthony Twesigye <twesigye10@gmail.com>
2022-07-22 09:59:51 +0300

    write output

--- select_features_by_layer.py
+++ select_features_by_layer.py
@@ -15,4 +15,7 @@ df_data_host_community = gdp.read_file(filename="inputs/UGA_Host_subcounty.gpkg"
 df_selection_19d = df_data_19d.overlay(right=df_data_host_community, how="identity")
 df_selection_177 = df_data_177.overlay(right=df_data_host_community, how="identity")
 
+# write output
+df_selection_19d.to_file("outputs/19d_buildings_ref_host.gpkg", layer='19d_buildings_ref_host', driver="GPKG")
+df_selection_177.to_file("outputs/177_buildings_ref_host.gpkg", layer='177_buildings_ref_host', driver="GPKG")
 
