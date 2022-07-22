50a99c8918cf97c734c2d5e577dda0520cc25a63
Anthony Twesigye <twesigye10@gmail.com>
2022-07-22 14:49:24 +0300

    testing with small data and reprojecting final selection before outputting

--- select_features_by_layer.py
+++ select_features_by_layer.py
@@ -1,6 +1,8 @@
 import geopandas as gdp
 
 # buildings datasets
+# df_data_test = gdp.read_file(filename="outputs/19d_buildings_test.gpkg")
+
 df_data_19d = gdp.read_file(filename="outputs/19d_buildings.gpkg")
 
 df_data_177 = gdp.read_file(filename="outputs/177_buildings.gpkg")
@@ -9,12 +11,17 @@ df_data_177 = gdp.read_file(filename="outputs/177_buildings.gpkg")
 
 df_data_host_community = gdp.read_file(filename="inputs/UGA_Host_subcounty.gpkg")
 df_data_host_community_reproject = df_data_host_community.to_crs(df_data_19d.crs)
+# df_data_host_community_reproject = df_data_host_community.to_crs(df_data_test.crs)
+
 # select by another layer
+# df_selection_test = df_data_test.overlay(right=df_data_host_community_reproject, how="difference")
+
 df_selection_19d = df_data_19d.overlay(right=df_data_host_community_reproject, how="identity")
 
 df_selection_177 = df_data_177.overlay(right=df_data_host_community_reproject, how="identity")
 
 # write output
-df_selection_19d.to_file("outputs/19d_buildings_ref_host.gpkg", layer='19d_buildings_ref_host', driver="GPKG")
-df_selection_177.to_file("outputs/177_buildings_ref_host.gpkg", layer='177_buildings_ref_host', driver="GPKG")
+# df_selection_test.to_crs(df_data_host_community.crs).to_file("outputs/test_buildings_ref_host.gpkg", layer='test_buildings_ref_host', driver="GPKG")
+df_selection_19d.to_crs(df_data_host_community.crs).to_file("outputs/19d_buildings_ref_host.gpkg", layer='19d_buildings_ref_host', driver="GPKG")
+df_selection_177.to_crs(df_data_host_community.crs).to_file("outputs/177_buildings_ref_host.gpkg", layer='177_buildings_ref_host', driver="GPKG")
 
