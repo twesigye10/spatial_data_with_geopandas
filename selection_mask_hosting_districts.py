60bb21aa0c1c8db229508cc12a4ed8c8040c1b0c
Anthony Twesigye <twesigye10@gmail.com>
2023-08-08 09:22:05 +0300

    updated osm dataset for 2023

--- selection_mask_hosting_districts.py
+++ selection_mask_hosting_districts.py
@@ -2,7 +2,7 @@ import geopandas as gdp
 
 df_data_host_community_reproj = gdp.read_file(filename="outputs/ref_host_districts.gpkg")
 print("Finished reading data")
-df_data_osm = gdp.read_file(filename="inputs/uganda_osm_09_2022/gis_osm_buildings_a_free_1.shp", mask=df_data_host_community_reproj)
+df_data_osm = gdp.read_file(filename="inputs/uganda_osm_08_2023/gis_osm_buildings_a_free_1.shp", mask=df_data_host_community_reproj)
 print("Finished reading file: gis_osm_buildings")
 df_data_osm.to_file("outputs/osm_buildings_host_districts_kampala.gpkg", layer='osm_buildings_ref_host', driver="GPKG")
 print("***       Finished processing osm_buildings         ***")
