b83f97f46365056c74ab415007d078bfa1a4159d
Anthony Twesigye <twesigye10@gmail.com>
2024-05-27 10:57:47 +0300

    updated combining files using pd.concat instead of append

--- combine_layers.py
+++ combine_layers.py
@@ -6,7 +6,7 @@ print("Finished reading file: 177_buildings")
 df_data_19d = gdp.read_file(filename="outputs/19d_buildings_ref_districts_kampala.gpkg")
 print("Finished reading file: 19d_buildings")
 
-combined_layers = df_data_177.append(df_data_19d)
+combined_layers = pd.concat([df_data_177, df_data_19d])
 print("Finished combining files")
 
 combined_layers.to_file("outputs/UGA_google_buildings_ref_host_kampala.gpkg", layer='uga_google_buildings_ref_host_kampala', driver="GPKG")
